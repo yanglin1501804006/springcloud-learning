1d0b861cff3b169f2f816dc765d484d1369094e4
yanglin1501804006 <41791609+yanglin1501804006@users.noreply.github.com>
2020-12-01 16:24:12 +0800

    Add files via upload

---
+++ seata-order-service/src/main/java/com/allan/cloud/service/StorageService.java
@@ -0,0 +1,17 @@
+package com.allan.cloud.service;
+
+import com.allan.cloud.domain.CommonResult;
+import com.macro.cloud.domain.CommonResult;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@FeignClient(value = "seata-storage-service")
+public interface StorageService {
+
+    /**
+     * 扣减库存
+     */
+    @GetMapping(value = "/storage/decrease")
+    CommonResult decrease(@RequestParam("productId") Long productId, @RequestParam("count") Integer count);
+}
