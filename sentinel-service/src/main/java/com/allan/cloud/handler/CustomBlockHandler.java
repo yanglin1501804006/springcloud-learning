9781259c619cb2197c19fe522ddc59e7b799267f
yanglin1501804006 <41791609+yanglin1501804006@users.noreply.github.com>
2020-12-01 16:27:00 +0800

    Add files via upload

---
+++ sentinel-service/src/main/java/com/allan/cloud/handler/CustomBlockHandler.java
@@ -0,0 +1,15 @@
+package com.allan.cloud.handler;
+
+import com.alibaba.csp.sentinel.slots.block.BlockException;
+import com.allan.cloud.domain.CommonResult;
+import com.macro.cloud.domain.CommonResult;
+
+/**
+ * Created by macro on 2019/11/7.
+ */
+public class CustomBlockHandler {
+
+    public CommonResult handleException(BlockException exception){
+        return new CommonResult("自定义限流信息",200);
+    }
+}
